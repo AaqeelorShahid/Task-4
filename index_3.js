ac0f9c69e571a70b3fdd38790a742b359fc6baf5
Shahid Aaqeel <45213200+AaqeelorShahid@users.noreply.github.com>
2021-02-15 18:23:38 +0530

    Initial Commit

---
+++ index_3.js
@@ -0,0 +1,21 @@
+const MongoClient = require("mongodb").MongoClient;
+
+const uri = "mongodb://127.0.0.1:27017"
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+(async () => {
+    await client.connect();
+    const db = client.db("Global-Warming_2")
+    const collection = db.collection("temp_info")
+    console.log(`Successfully connected to ${db.databaseName} database`);
+
+    let query = {$and:[{averageTemp: {$gt: 70 }}, {averageTemp: {$lt: 134}}]}
+    let document = await collection.find(query)
+    while (await document.hasNext()){
+        let nextD = await document.next();
+        console.log(nextD);
+    }
+
+    client.close()
+})();
+
